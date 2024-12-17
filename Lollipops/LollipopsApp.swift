8fd5494626261eeedba0da79199fc417d6a199a2
keshew <artyom.korotckov2017@yandex.ru>
2024-12-18 03:17:54 +0500

    Initial Commit

---
+++ Lollipops/LollipopsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LollipopsApp.swift
+//  Lollipops
+//
+//  Created by Артём Коротков on 18.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct LollipopsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
